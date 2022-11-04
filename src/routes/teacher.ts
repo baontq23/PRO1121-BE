047c34d96bc3731f4361968ada0073763967ad8d
baontq23 <baooshacker@gmail.com>
2022-11-04 23:01:52 +0700

    feat: login via email

--- src/routes/teacher.ts
+++ src/routes/teacher.ts
@@ -8,9 +8,10 @@ router.get('/', TeacherController.listAll);
 
 // Get one user
 router.get('/:id([0-9]+)', TeacherController.getOneById);
+router.get('/login/:email', TeacherController.getOneByEmail);
 
 //Create a new user
-router.post('/', TeacherController.newUser);
+router.post('/', TeacherController.newTeacher);
 
 //Edit one user
 router.patch('/', TeacherController.editUser);
