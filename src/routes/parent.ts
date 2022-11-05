fdfa3052743a3ffdf5589f4c99abf4ad73de6a02
nguyentuan1612 <tuanbeo203@gmail.com>
2022-11-05 15:10:32 +0700

    feat: Methods Of Parent

--- src/routes/parent.ts
+++ src/routes/parent.ts
@@ -1,5 +1,26 @@
 import { Router } from "express";
+import ParentController from '../controllers/ParentController';
 
 const router = Router();
+//Get all Parents
+router.get('/', ParentController.listAll);
+
+//Create a new Parent
+router.post('/', ParentController.newParent);
+
+//Edit one Parent
+router.patch('/', ParentController.editParent);
+
+//Delete one Parent
+router.delete('/:id([0-9]+)', ParentController.deleteParent);
+
+//ChangePassword one Parent
+router.patch('/changePassword/:id([0-9]+)', ParentController.changePassword);
+
+//get one by email
+router.get('/login/:email', ParentController.getOneByEmail);
+
+//login by Phone
+router.get('/login', ParentController.loginByPhone);
 
 export default router;
