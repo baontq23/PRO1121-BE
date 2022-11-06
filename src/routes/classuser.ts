e7452581131a207c168c9230481481cd3faf09c9
nguyentuan1612 <tuanbeo203@gmail.com>
2022-11-06 21:44:43 +0700

    feat: methods of classroom student

--- src/routes/classuser.ts
+++ src/routes/classuser.ts
@@ -1,5 +1,18 @@
 import { Router } from "express";
-
+import ClassroomStudentController from '../controllers/ClassroomStudentController';
 const router = Router();
+
+//Get all ClassRoomStudent
+router.get('/', ClassroomStudentController.listAll);
+
+//Create a new ClassRoomStudent
+router.post('/', ClassroomStudentController.newClassStudent);
+
+//Edit one ClassRoomStudent
+router.patch('/', ClassroomStudentController.editClassStudent);
+
+//Delete one ClassRoomStudent
+router.delete('/:id([0-9]+)', ClassroomStudentController.deleteClassStudent);
+
 
 export default router;
