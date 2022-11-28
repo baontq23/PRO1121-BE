0bfc2c1b02d09f0ebea0de3e8ffa446c060f1e79
baontq23 <baooshacker@gmail.com>
2022-11-28 19:24:53 +0700

    fix: student res

--- src/controllers/ClassroomStudentController.ts
+++ src/controllers/ClassroomStudentController.ts
@@ -2,7 +2,6 @@ import { validate } from 'class-validator';
 import { Request, Response } from 'express';
 import { AppDataSource } from '../data-source';
 import { ClassStudent } from '../entity/ClassStudent';
-import { Parent } from '../entity/Parent';
 
 class ClassroomController {
   static listAll = async (req: Request, res: Response) => {
@@ -31,8 +30,8 @@ class ClassroomController {
     const queryRunner = AppDataSource.manager;
     const academictranscript = await queryRunner.query(
       'SELECT tbl_class_students.id, tbl_classrooms.id AS classroom_id,tbl_students.id AS student_id, tbl_students.name AS student_name,tbl_class_students.regularScore1 AS regular_score_1,tbl_class_students.regularScore2  AS regular_score_2,tbl_class_students.regularScore3 AS  regular_score_3,tbl_class_students.midtermScore AS  midterm_score,tbl_class_students.finalScore  AS  final_score, tbl_class_students.semester  FROM tbl_class_students INNER JOIN tbl_students  ON tbl_class_students.student_id = tbl_students.id INNER JOIN tbl_classrooms  ON tbl_class_students.classroom_id = tbl_classrooms.id  WHERE tbl_classrooms.id = "' +
-      idClass +
-      '"'
+        idClass +
+        '"'
     );
     if (academictranscript.length === 0) {
       res.status(404).send({
@@ -53,11 +52,11 @@ class ClassroomController {
     const idClass = parseInt(req.params.idClass);
     const queryRunner = AppDataSource.manager;
     const getAllInformation = await queryRunner.query(
-      'SELECT tbl_students.name AS student_name ,tbl_students.dob AS student_dob ,tbl_students.gender AS student_gender,tbl_class_students.regularScore1 AS regular_score_1,tbl_class_students.regularScore2 AS regular_score_2 ,tbl_class_students.regularScore3 AS regular_score_3,tbl_class_students.midtermScore AS midterm_score,tbl_class_students.finalScore AS final_score, tbl_class_students.semester,tbl_parents.name AS parent_name ,tbl_parents.email AS parent_email,tbl_parents.dob AS parent_dob,tbl_parents.phone AS parent_phone FROM tbl_class_students INNER JOIN tbl_students  ON tbl_class_students.student_id = tbl_students.id INNER JOIN tbl_classrooms  ON tbl_class_students.classroom_id = tbl_classrooms.id INNER JOIN tbl_parents ON tbl_parents.id = tbl_students.parent_id   WHERE tbl_students.id = "' +
-      id +
-      '" AND tbl_classrooms.id = "' +
-      idClass +
-      '"'
+      'SELECT tbl_students.name AS student_name ,tbl_students.dob AS student_dob ,tbl_students.gender AS student_gender,tbl_class_students.regularScore1 AS regular_score_1,tbl_class_students.regularScore2 AS regular_score_2 ,tbl_class_students.regularScore3 AS regular_score_3,tbl_class_students.midtermScore AS midterm_score,tbl_class_students.finalScore AS final_score, tbl_class_students.semester,tbl_parents.name AS parent_name ,tbl_parents.email AS parent_email,tbl_parents.dob AS parent_dob,tbl_parents.phone AS parent_phone,tbl_parents.fcmToken AS parent_fcmtoken FROM tbl_class_students INNER JOIN tbl_students  ON tbl_class_students.student_id = tbl_students.id INNER JOIN tbl_classrooms  ON tbl_class_students.classroom_id = tbl_classrooms.id INNER JOIN tbl_parents ON tbl_parents.id = tbl_students.parent_id   WHERE tbl_students.id = "' +
+        id +
+        '" AND tbl_classrooms.id = "' +
+        idClass +
+        '"'
     );
     if (getAllInformation.length === 0) {
       res.status(404).send({
@@ -70,13 +69,16 @@ class ClassroomController {
     }
   };
 
-
   static deleteStudentInClassUser = async (req: Request, res: Response) => {
     const idStudent = req.params.idStudent;
     const idClassRoom = parseInt(req.params.idClass);
     const queryRunner = AppDataSource.manager;
-   const deleteClassStudent = await queryRunner.query(
-   'DELETE FROM tbl_class_students  WHERE tbl_class_students.student_id = "'+idStudent+'" AND tbl_class_students.classroom_id = "'+idClassRoom+'"'
+    const deleteClassStudent = await queryRunner.query(
+      'DELETE FROM tbl_class_students  WHERE tbl_class_students.student_id = "' +
+        idStudent +
+        '" AND tbl_class_students.classroom_id = "' +
+        idClassRoom +
+        '"'
     );
     if (deleteClassStudent.length === 0) {
       res.status(404).send({
